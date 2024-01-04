7da259b316c2ec056dec41f0877fca5cada0d8d6
Millena Limaverde <millenalimaverde@outlook.com>
2024-01-04 20:25:05 -0300

    feat: 🎸 atualização timer, reorganização

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+export function Home() {
+  return (
+    <h1>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt placeat
+      doloribus id alias corporis magnam totam quod aliquid illo distinctio a
+      tempore nulla dolorum, numquam officiis, possimus, repellendus velit
+      vitae!
+    </h1>
+  )
+}
