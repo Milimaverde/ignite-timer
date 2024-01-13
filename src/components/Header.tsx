a7c8403b544e8122d35cd436b2b0c82604c8d2a9
Millena Limaverde <millenalimaverde@outlook.com>
2024-01-12 22:09:02 -0300

    feat: 🎸 adicionando layouts

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <h1>Header</h1>
+    // <Container>
+    //   <Content>
+    //     <Logo />
+    //     <Menu />
+    //   </Content>
+    // </Container>
+  )
+}
